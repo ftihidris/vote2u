394dfef39d04c53962db58110d4f425373249651
ftihidris <fatihidris13@gmail.com>
2024-03-31 03:42:30 +0800

    voting pages

---
+++ lib/utils/constants.dart
@@ -0,0 +1,11 @@
+String infura_url =
+    "https://sepolia.infura.io/v3/1fe6004dfd394d35be78314d44fcab0f";
+
+String owner_private_key =
+    "7e488dc8f1f60c7796bc0f2d3a03caddac5b6b9635fdebff97018f6ec3e7f96c";
+
+String voter_private_key =
+    "b41f68564dc81ebc6ec71c4b809e203d69934cb1aa5ec73fdf9f8ef649577188";
+
+String contractAddress1 = 
+    "0x5a542f121ea1Bd2019D1ACF4B1Fd2C14D22f932C";
