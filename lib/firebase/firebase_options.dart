cbfb1bf77b7f5e39251ce1f3a8a4c27533eb9889
ftihidris <fatihidris13@gmail.com>
2024-08-22 15:51:33 +0800

    new update

--- lib/firebase/firebase_options.dart
+++ lib/firebase/firebase_options.dart
@@ -31,4 +31,40 @@ class DefaultFirebaseOptions {
         );
     }
   }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyBG3jihSvrwaCok89ozLuPDJxma329-YBQ',
+    appId: '1:1013674080696:web:ea08613a4d4c38699eb74d',
+    messagingSenderId: '1013674080696',
+    projectId: 'vote2u-6d272',
+    authDomain: 'vote2u-6d272.firebaseapp.com',
+    storageBucket: 'vote2u-6d272.appspot.com',
+    measurementId: 'G-Q5R36CFD5N',
+  );
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyDws0vdCwHZD2Bv1-4Tmk9-pHgybSVM5o0',
+    appId: '1:1013674080696:android:99c6bd8d938534bc9eb74d',
+    messagingSenderId: '1013674080696',
+    projectId: 'vote2u-6d272',
+    storageBucket: 'vote2u-6d272.appspot.com',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'AIzaSyAUMlxNXwPhneJi3hejh8n5hqhWJh4BWvk',
+    appId: '1:1013674080696:ios:d9dd03910b78a3d79eb74d',
+    messagingSenderId: '1013674080696',
+    projectId: 'vote2u-6d272',
+    storageBucket: 'vote2u-6d272.appspot.com',
+    iosBundleId: 'com.example.vote2u',
+  );
+
+  static const FirebaseOptions macos = FirebaseOptions(
+    apiKey: 'AIzaSyAUMlxNXwPhneJi3hejh8n5hqhWJh4BWvk',
+    appId: '1:1013674080696:ios:64mediumSizeBox58234377004b9eb74d',
+    messagingSenderId: '1013674080696',
+    projectId: 'vote2u-6d272',
+    storageBucket: 'vote2u-6d272.appspot.com',
+    iosBundleId: 'com.example.vote2u.RunnerTests',
+  );
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 //blockchain integration
 // ignore: non_constant_identifier_names
 String infura_url=
-    "your_infura_url";
+    "https://polygon-amoy.infura.io/v3/1fe6004dfd394d35be78314d44fcab0f";
 
 // ignore: non_constant_identifier_names
 String owner_private_key =
-    "your_owner_private_key";
+    "7e488dc8f1f60c7796bc0f2d3a03caddac5b6b9635fdebff97018f6ec3e7f96c";
 
 // ignore: non_constant_identifier_names
 String voter_private_key =
-    "your_voter_private_key";
+    "b41f68564dc81ebc6ec71c4b809e203d69934cb1aa5ec73fdf9f8ef649577188";
 
 //colour
 const darkPurple = Color.fromARGB(255, 63, 41, 120);
